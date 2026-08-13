08062817db7028376132c48e9ea8b3d9d821d37d
Leytox <iladevder@gmail.com>
2026-08-14 00:40:50 +0300

    fix: add upload module to search module to resolve an issues

--- apps/api/src/search/search.module.ts
+++ apps/api/src/search/search.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { SearchService } from './search.service';
 import { SearchController } from './search.controller';
 import { AuthModule } from '../auth/auth.module';
+import { UploadModule } from '../upload/upload.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UploadModule],
   controllers: [SearchController],
   providers: [SearchService],
 })
